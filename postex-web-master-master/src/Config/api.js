a7175cf066b1b6773fde40f7dac6ebeff1391936
Miguel Lima <miguelsousalima@hotmail.com>
2020-04-10 03:35:46 -0300

    Procfile heroku

--- postex-web-master-master/src/Config/api.js
+++ postex-web-master-master/src/Config/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "https://cors-anywhere.herokuapp.com/http://104.237.10.79:5022/" });
+const api = axios.create({ baseURL: "https://postex-full.herokuapp.com/" });
 const ApiCep = axios.create({ baseURL: `https://viacep.com.br/ws` });
 export default api;
 export { ApiCep };
